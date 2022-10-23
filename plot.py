c3336f65b04a4a3b8e9917c758bd6b90868959b2
maka <makagucci@gmx.com>
2022-10-23 05:41:29 +0100

    [+] housekeeping

--- plot.py
+++ plot.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import matplotlib.ticker as plticker
 from data import data as p
 
 x, y = [], []
@@ -11,20 +12,22 @@ for i in p:
 plt.figure(facecolor='#0A0226')
 plt.style.use('dark_background')
 
-plt.plot(x,y, '-.', c='#27DEF2', linewidth=3)
-
+plt.plot(x,y, '-.', c='#D621C1', lw=5)
 f0 = {'family':'Source Code Pro','color':'#27DEF2','size':40} 
 f1 = {'family':'Source Code Pro','color':'#D621C1','size':30}
 
-plt.title('xSushi Historical Ratio', fontdict=f0)
-plt.xlabel('Block', fontdict=f1)
-plt.ylabel('Ratio', fontdict=f1)
+plt.title('xSushi Historical Ratio', pad=20, fontdict=f0)
+plt.xlabel('Block', labelpad=10, fontdict=f1)
+plt.ylabel('Ratio', labelpad=10, fontdict=f1)
 
 # make readable
 plt.tick_params(axis='both', which='major', labelsize=15)
 
 plt.xticks(rotation=75)
 ax = plt.gca()
+ax.set_facecolor('#100459')
+
+plt.grid(1, ls=':', c='#27DEF2', lw='.2')
 
 for label in ax.get_xaxis().get_ticklabels()[::2]:
   label.set_visible(False)
@@ -32,7 +35,4 @@ for label in ax.get_xaxis().get_ticklabels()[::2]:
 for label in ax.get_yaxis().get_ticklabels()[::2]:
   label.set_visible(False)
 
-ax.set_facecolor('#100459')
-
-#plt.savefig('chart.png', facecolor='#0A0226', dpi=300)
 plt.show()
